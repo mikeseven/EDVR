8bc15de2a6758f7a057fc22d84d8857504cfa8f9
Mikael Sevenier <mikeseven@gmail.com>
2019-07-11 13:13:05 -0700

    update CharbonnierLoss to be closer to other losses

--- codes/models/modules/loss.py
+++ codes/models/modules/loss.py
@@ -5,11 +5,12 @@ import torch.nn as nn
 class CharbonnierLoss(nn.Module):
     """Charbonnier Loss (L1)"""
 
-    def __init__(self, eps=1e-6):
+    def __init__(self, eps=1e-3, reduction='mean'):
         super(CharbonnierLoss, self).__init__()
-        self.eps = eps
+        self.eps = eps*eps
+        self.reduction_fn = torch.mean if reduction == 'mean' else torch.sum
 
     def forward(self, x, y):
         diff = x - y
-        loss = torch.mean(torch.sqrt(diff * diff + self.eps))
+        loss = self.reduction_fn(torch.sqrt(diff * diff + self.eps))
         return loss
